e5e79d5a7093ec810dfd85424013bdd162bc0390
damiandbz <damianbatiz@hotmail.com>
2025-09-28 19:12:34 -0500

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'hello-pwa-cache-v1';
+const urlsToCache = [
+    'index.html',
+    'style.css',
+    'firstbackground.jpg',
+    'manifest.json'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            console.log('Opened cache');
+            return cache.addAll(urlsToCache);
+        })
+    );
+} );
+
+self.addEventListener('fetch', event =>{
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            // Return cache file if found, else fetch from network
+            return response || fetch (event.request);
+        })
+    );
+});
